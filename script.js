ffe064b5ea936e08519581891dbecf53707324ed
Artur Z <artur.zahorski@gmail.com>
2019-02-09 12:23:06 +0100

    fix bug with .attr href

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
 
   $.each(data, function(index){
     $(".row").append("<div id='column"+index+"' class='col-sm-12 col-md-6 col-lg-4 mb-3'><figure class='border-5'><a href=''><img class='img-fluid w-100' src=''></a><p class='text-justify text-white'></p></figure></div>");
-    $("#column"+index+" a").attr("html", data[index].link);
+    $("#column"+index+" a").attr("href", data[index].link);
     $("#column"+index+" img").attr("src", data[index].img);
     $("#column"+index+" p").html(data[index].title);
   }); 
